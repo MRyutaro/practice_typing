e8bf7873c7163da7d401b64862f722406f1ff8e0
RyutaroMatsumoto <m2002r1028@icloud.com>
2023-02-01 22:13:24 +0900

    [fix] 変数名をスネークケースに変更した

--- main.js
+++ main.js
@@ -1,66 +1,35 @@
 
 const url = "./words.json";
 
-let Q, Q_No, Q_i, Q_l;
-let inputed_Character_Total_Num;
-let correctly_Inputed_Character_Total_Num;
-let correctly_Input_Rate;
+let chars_for_display;
+let chars_for_decision;
+let word_num;
+let char_check_flag;
+let word_length;
+let inputed_char_total_num = 0;
+let correctly_inputed_char_total_num = 0;
+let correctly_inputed_rate = 0;
 
-window.addEventListener("load", load_Json);
-window.addEventListener("keypress", push_Keydown);
+window.addEventListener("load", load_json);
+window.addEventListener("keypress", push_keydown);
 
-function load_Json(){
-  console.log("load json...");
-  fetch(url)
-    .then( response => response.json())
-    .then( data => decide_Words(data));
+function load_json(){
+  changeLanguageCode(0);
+  make_new_word(chars_for_decision, chars_for_display);
 };
 
-function decide_Words(data){
-  Q = data.Japanese;
-  Q_No = Math.floor( Math.random() * Q.length)
-  Q_i = 0;
-  Q_l = Q[Q_No].romaji.length;
+function make_new_word(chars_for_decision, chars_for_display){
+  word_num = Math.floor( Math.random() * chars_for_decision.length);
+  word_length = chars_for_decision[word_num].length;
+  char_check_flag = 0;
+  show_text(chars_for_decision, chars_for_display, char_check_flag, word_length);
+  return word_num, char_check_flag, word_length;
 };
 
-function push_Keydown(event){
-  inputed_Character_Total_Num++;
-  let keyCode = event.key;
-  console.log("文字：", Q[Q_No].romaji, "、文字番号：", Q_i, "、文字長さ：",Q_l);
-	if (keyCode == " "){
-    console.log("初期化");
-    inputed_Character_Total_Num = 0;
-    correctly_Inputed_Character_Total_Num = 0;
-    correctly_Input_Rate = 0;
-    makeNewWord(Q);
-	};
-  
-	if (Q[Q_No].romaji.charAt(Q_i) == keyCode) { //押したキーが合っていたら
-    correctly_Inputed_Character_Total_Num++;
-    console.log("score", correctly_Inputed_Character_Total_Num);
-		Q_i++; //判定する文章に１足す
-    
-    console.log("キー合ってた");
-    document.getElementById("start").innerHTML = Q[Q_No]._.substring(0, Q_l); //問題を書き出す
-		document.getElementById("romaji").innerHTML = Q[Q_No].romaji.substring(Q_i, Q_l); //問題を書き出す
+function show_text(chars_for_decision, chars_for_display, start_char_num, end_char_num){
+  document.getElementById("start").innerHTML = chars_for_decision[word_num].substring(0, end_char_num);
+  document.getElementById("romaji").innerHTML = chars_for_display[word_num].substring(start_char_num, end_char_num);
 
-		if (Q_l-Q_i == 0){ //全部正解したら
-      console.log("単語変更");
-      makeNewWord(Q);
-		};
-	};
-
-  showScore();
-
-};
-
-function makeNewWord(Q){
-  Q_No = Math.floor( Math.random() * Q.length);//問題をランダムで出題する
-  Q_i = 0;//回答初期値・現在どこまで合っているか判定している文字番号
-  Q_l = Q[Q_No].romaji.length;//計算用の文字の長さ
-  document.getElementById("start").innerHTML = Q[Q_No]._.substring(Q_i, Q_l); //新たな問題を書き出す
-  document.getElementById("romaji").innerHTML = Q[Q_No].romaji.substring(Q_i, Q_l); //新たな問題を書き出す
-  return Q_No, Q_i, Q_l
 };
 
 function changeLanguageCode(language_code){
@@ -71,23 +40,52 @@ function changeLanguageCode(language_code){
 
 function changeLanguage(language_code, json){
   if (language_code == 0){
-    document.getElementById("changeLanguageText").innerHTML = "日本語モードです";
-    Q = json.Japanese;
+    document.getElementById("change_language_text").innerHTML = "日本語モードです";
+    chars_for_decision = json.Japanese.romaji;
+    chars_for_display = json.Japanese._;
   } 
   else if(language_code == 1){
-    document.getElementById("changeLanguageText").innerHTML = "英語モードです";
-    Q = json.English;
+    document.getElementById("change_language_text").innerHTML = "英語モードです";
+    chars_for_decision = json.English;
+    chars_for_display = json.English;
   };
 };
 
-function showScore(){
-  document.getElementById("inputed_Character_Total_Num").innerHTML = inputed_Character_Total_Num;
-  document.getElementById("correctly_Inputed_Character_Total_Num").innerHTML = correctly_Inputed_Character_Total_Num;
-  if(inputed_Character_Total_Num != 0){
-    correctly_Input_Rate = Math.round((correctly_Inputed_Character_Total_Num/inputed_Character_Total_Num)*100);
-  } 
-  else{
-    correctly_Input_Rate = 0;
-  }
-  document.getElementById("correctly_Input_Rate").innerHTML = correctly_Input_Rate + "%";
+function push_keydown(event){
+  inputed_char_total_num += 1;
+  let key_code = event.key;
+  console.log("文字：", chars_for_display, "、文字番号：", char_check_flag, "、文字長さ：",word_length);
+	if (key_code == " "){
+    inputed_char_total_num = 0;
+    console.log("初期化");
+    correctly_inputed_char_total_num = 0;
+    correctly_inputed_rate = 0;
+    make_new_word(chars_for_decision, chars_for_display, char_check_flag, word_length);
+	};
+  
+	if (key_code == chars_for_decision[word_num].charAt(char_check_flag)) {
+    correctly_inputed_char_total_num += 1;
+    console.log("score", correctly_inputed_char_total_num);
+		char_check_flag++;
+    
+    console.log("キー合ってた");
+    show_text(chars_for_decision, chars_for_display, char_check_flag, word_length);
+    
+		if (word_length-char_check_flag == 0){
+      console.log("単語変更");
+      make_new_word(chars_for_decision, chars_for_display);
+		};
+	};
+  
+  show_score();
+
+};
+
+function show_score(){
+  document.getElementById("inputed_char_total_num").innerHTML = inputed_char_total_num;
+  document.getElementById("correctly_inputed_char_total_num").innerHTML = correctly_inputed_char_total_num;
+  if(inputed_char_total_num != 0){
+    correctly_inputed_rate = Math.round((correctly_inputed_char_total_num/inputed_char_total_num)*100);
+  };
+  document.getElementById("correctly_inputed_rate").innerHTML = correctly_inputed_rate + "%";
 };
